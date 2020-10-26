4cc0387e71233ef9d4f29b5c2f9afbe7cb41146d
KeiMichell <krockdale611@gmail.com>
2020-10-26 13:22:05 +0900

    record-feature

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,21 @@
 class RecordsController < ApplicationController
 
   def index
-    @user
+    @records = Record.all #@recordsというインスタンス変数にrecordsテーブルのレコードを全て代入
   end
 
   def new
-    @record = Record.new
+    @record = Record.new #creating a new record
+  end
+
+  def create
+    Record.create(kilos: record_params[:kilos], reps: record_params[:reps], user_id: current_user.id) #create record with user_id
+    @records = Record.where(user_id: current_user.id)
+  end
+
+
+  private
+  def record_params
+    params.require(:record).permit(:kilos, :reps)
   end
 end
