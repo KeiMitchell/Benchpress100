bdcc9d1616147f00bf28e6fdce0991b7c89b2efc
KeiMichell <krockdale611@gmail.com>
2020-10-26 15:25:45 +0900

    最大挙重量の計算・表示

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,12 +5,12 @@ class RecordsController < ApplicationController
   end
 
   def new
-    @record = Record.new #creating a new record
+    @record = Record.new
   end
 
   def create
     Record.create(kilos: record_params[:kilos], reps: record_params[:reps], user_id: current_user.id) #create record with user_id
-    @record = Record.order(updated_at: :desc).limit(1)
+    @record = Record.order(updated_at: :desc).limit(1) #最新のレコードを取得し、@recordインスタンス変数に代入
     @max = max_record
     @records = Record.where(user_id: current_user.id)
 
@@ -26,7 +26,6 @@ class RecordsController < ApplicationController
   def max_record
     kilos = @record.pluck(:kilos).join.to_i
     reps = @record.pluck(:reps).join.to_i
-    max = (kilos * (1 + (reps / 40.0)))
-    return max.round(4)
+    max = (kilos * (1 + (reps / 40.0))) #最大挙重量の計算式
   end
 end
